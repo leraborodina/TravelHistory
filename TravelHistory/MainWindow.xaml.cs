7b28d8ce77220b2d16e83d23bf7d3a3e73973d73
leraborodina <valeraborodina29@gmail.com>
2023-05-23 11:34:15 +0300

    Добавьте файлы проекта.

---
+++ TravelHistory/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TravelHistory
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Registration_window_open(object sender, RoutedEventArgs e)//закрытие окна по кнопке
+        {
+            RegistrationWindow RegistrationWindow = new RegistrationWindow();
+            RegistrationWindow.Show();
+            this.Close();
+        }
+
+        private void Loginpage_close_button_click(object sender, RoutedEventArgs e)//переход на окно регистрации
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
